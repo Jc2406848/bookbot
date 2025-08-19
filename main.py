96e1b2c31d50e5d5cbbedc5264e627a6c5a65207
Jc2406848 <jc_monk@hotmail.co.uk>
2025-08-19 15:08:53 +0100

    added count_characters, counts up each occurrence of each character in the passed in string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words
+from stats import count_characters
 
 def get_book_text(filepath = ""):
     with open(filepath) as f:
@@ -6,7 +7,10 @@ def get_book_text(filepath = ""):
         return file
     
 def main():
-    num_words = count_words(get_book_text("books/frankenstein.txt"))
+    book = get_book_text("books/frankenstein.txt")
+    num_words = count_words(book)
     print(f"{num_words} words found in the document")
+    characters = count_characters(book)
+    print(characters)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def count_words(book_text = ""):
     book_list = book_text.split()
     word_count = len(book_list)
     return word_count
+
+def count_characters(book_text = ""):
+    book = book_text.lower()
+    characters = {}
+    for character in book:
+        if character in characters:
+            characters[character] += 1
+        else:
+            characters[character] = 1
+    return characters
