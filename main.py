9f9707d19a3b7dfa1e9cf8dd0434830e884d7e91
Jc2406848 <jc_monk@hotmail.co.uk>
2025-08-19 16:24:25 +0100

    changed to use arguments for path to book

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import count_characters
 from stats import list_characters
@@ -8,20 +9,24 @@ def get_book_text(filepath = ""):
         return file
     
 def main():
-    location = "books/frankenstein.txt"
-    book = get_book_text(location)
-    num_words = count_words(book)
-    characters = count_characters(book)
-    sorted_characters = (list_characters(characters))
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        location = sys.argv[1]
+        book = get_book_text(location)
+        num_words = count_words(book)
+        characters = count_characters(book)
+        sorted_characters = (list_characters(characters))
 
-    print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {location}...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for char in sorted_characters:
-        if char["char"].isalpha():
-            print(f"{char["char"]}: {char["num"]}")
-    print("============= END ===============")
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {location}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_words} total words")
+        print("--------- Character Count -------")
+        for char in sorted_characters:
+            if char["char"].isalpha():
+                print(f"{char["char"]}: {char["num"]}")
+        print("============= END ===============")
 
 main()
